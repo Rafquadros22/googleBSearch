83adb3b6c22c3317bdaba2d065af4b3c074c9f98
rafael <rafquadros22@gmail.com>
2020-08-09 20:56:03 -0400

    4th commit

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -12,7 +12,7 @@ function Form({ q, handleInputChange, handleFormSubmit }) {
           id="Title"
           type="text"
           value={q}
-          placeholder="Ready Player One"
+          placeholder="Book-Search"
           name="q"
           onChange={handleInputChange}
           required
